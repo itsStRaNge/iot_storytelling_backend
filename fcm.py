1065c9ad924a40b07fe2971da9f3f15b08fab4d5
Lukas Bernhard <L.J.Bernhard@web.de>
2018-09-17 12:35:15 +0200

    ..

--- fcm.py
+++ fcm.py
@@ -1,6 +1,6 @@
 from pyfcm import FCMNotification
 
-#https://github.com/olucurious/PyFCM
+# https://github.com/olucurious/PyFCM
 
 API_KEY = "AAAAjA9yJPo:APA91bGpa13pfTdiiWS4wEKpH_ougbCbK15oDrbatFhxo3wtiv39fIgN41RVignrHWmrDVB3tvTxZLK7K41D9dAD6xNyrX-7dJgVSIIqjapeMf8yEvBL1nWW_YtsHL9OmzWHotYsSwMV"
 push_service = FCMNotification(api_key=API_KEY)
