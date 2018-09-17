e11b8fcc49ebf103b92ff5a6a539dd09141cad1b
Lukas Bernhard <L.J.Bernhard@web.de>
2018-09-17 11:12:51 +0200

    successful communication to actuator app via firebase

---
+++ fcm.py
@@ -0,0 +1,6 @@
+from pyfcm import FCMNotification
+API_KEY = "AAAAjA9yJPo:APA91bGpa13pfTdiiWS4wEKpH_ougbCbK15oDrbatFhxo3wtiv39fIgN41RVignrHWmrDVB3tvTxZLK7K41D9dAD6xNyrX-7dJgVSIIqjapeMf8yEvBL1nWW_YtsHL9OmzWHotYsSwMV"
+push_service = FCMNotification(api_key=API_KEY)
+
+result = push_service.notify_topic_subscribers(topic_name="news", message_body=" THIS IS PYTHOOOON!!!")
+print(result)
