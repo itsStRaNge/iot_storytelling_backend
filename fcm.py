b236e6fe093c01675c756946b74e79f139cf7e30
Lukas Bernhard <L.J.Bernhard@web.de>
2018-09-17 13:34:25 +0200

    working on merge for tcp server and push notifications

--- fcm.py
+++ fcm.py
@@ -3,7 +3,9 @@ from pyfcm import FCMNotification
 # https://github.com/olucurious/PyFCM
 
 API_KEY = "AAAAjA9yJPo:APA91bGpa13pfTdiiWS4wEKpH_ougbCbK15oDrbatFhxo3wtiv39fIgN41RVignrHWmrDVB3tvTxZLK7K41D9dAD6xNyrX-7dJgVSIIqjapeMf8yEvBL1nWW_YtsHL9OmzWHotYsSwMV"
-push_service = FCMNotification(api_key=API_KEY)
 
-result = push_service.notify_topic_subscribers(topic_name="news", message_body=" THIS IS PYTHOOOON!!!")
-print(result)
+def push_event(msg, type="news"):
+    push_service = FCMNotification(api_key=API_KEY)
+
+    result = push_service.notify_topic_subscribers(topic_name=type, message_body=msg)
+    print(result)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import fcm
 
 HOST = ''  # Symbolic name, meaning all available interfaces
 PORT = 8888  # Arbitrary non-privileged port
@@ -14,24 +15,22 @@ except socket.error as msg:
     print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
 
-print('Socket bind complete')
-
 # Start listening on socket
-s.listen(10)
+s.listen()
 print('Socket now listening')
 
 # now keep talking with the client
-while 1:
+while True:
     try:
         # wait to accept a connection - blocking call
         conn, addr = s.accept()
         print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
         while True:
-
             # Receiving from client
             data = conn.recv(1024)
             print(data)
+            fcm.push_event(str(data))
             if not data:
                 break
     except Exception:
