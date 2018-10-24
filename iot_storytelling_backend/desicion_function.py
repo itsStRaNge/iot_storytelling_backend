081969188559d566171a186303f9b81636474d9c
Øyvind Sørdal Klungre <oklunger@online.no>
2018-10-24 10:16:26 +0200

    first try of decision function for prototype 2

--- iot_storytelling_backend/desicion_function.py
+++ iot_storytelling_backend/desicion_function.py
@@ -1,7 +1,13 @@
+past_qrcode = ""
+
 def desfu(pos,qrcode):
 
     if qrcode == "kode1":
-        if pos == 1:
+        past_qrcode = 'kode1'
+        if past_qrcode == "kode1" and pos == 1:
+            if pos == 1:
+                pass
+        elif past_qrcode != "kode1" and pos == 1:
             song = "song1-1"
             picture = "picture1-1"
         elif pos == 2:
@@ -10,22 +16,43 @@ def desfu(pos,qrcode):
         elif pos == 3:
             song = "song1-3"
             picture = "picture1-3"
-        elif pos == 4:
-            song = "song1-4"
-            picture = "picture1-4"
+
 
     if qrcode == "kode2":
+        past_qrcode = 'kode2'
         if pos == 1:
             song = "song2-1"
             picture = "picture2-1"
         elif pos == 2:
             song = "song2-2"
-            picture = "picture2-1"
+            picture = "picture2-2"
         elif pos == 3:
             song = "song2-3"
             picture = "picture2-3"
-        elif pos == 4:
-            song = "song2-4"
-            picture = "picture2-4"
 
+    if qrcode == "kode3":
+        past_qrcode = 'kode3'
+        if pos == 1:
+            song = "song3-1"
+            picture = "picture3-1"
+        elif pos == 2:
+            song = "song3-2"
+            picture = "picture3-2"
+        elif pos == 3:
+            song = "song3-3"
+            picture = "picture3-3"
+
+    if qrcode == "kode4":
+        past_qrcode = 'kode4'
+        if pos == 1:
+            song = "song4-1"
+            picture = "picture4-1"
+        elif pos == 2:
+            song = "song4-2"
+            picture = "picture4-2"
+        elif pos == 3:
+            song = "song4-3"
+            picture = "picture4-3"
+
+    past_qrcode = qrcode
     return song, picture
